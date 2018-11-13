9f9500f0caf0368a098282e0c2925a97f0ebd055
masa Ishida <aaa@bbb.com>
2018-11-13 18:02:18 +0900

    画面

--- Project/IHWork/IHWork/deliveryList.Designer.cs
+++ Project/IHWork/IHWork/deliveryList.Designer.cs
@@ -59,7 +59,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(147, 39);
             this.button1.TabIndex = 2;
-            this.button1.Text = "納品書発行";
+            this.button1.Text = "請求書発行";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // deliveryList
