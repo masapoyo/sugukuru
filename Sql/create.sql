b0e0f84b022c41b6d2ddfbda67fa3be23c11c802
Yuki Yoshida <ohs60319@osaka.hal.ac.jp>
2018-11-13 16:33:04 +0900

    11/13回収まで設計分SQL

--- Sql/create.sql
+++ Sql/create.sql
@@ -16,8 +16,8 @@ CREATE TABLE m_our_company (
     INDEX(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-#M顧客テーブル
-CREATE TABLE m_customers (
+#T顧客テーブル
+CREATE TABLE t_customers (
     id INT(8) NOT NULL AUTO_INCREMENT,    #顧客ID
     phonetic VARCHAR(512),    #フリガナ
     name VARCHAR(256) NOT NULL,    #顧客名
@@ -25,6 +25,8 @@ CREATE TABLE m_customers (
     address VARCHAR(128),    #住所
     phone CHAR(12),  #電話番号
     fax CHAR(12),    #FAX番号
+    credit_limit DECIMAL(13, 0) NOT NULL DEFAULT 20000000,  #与信限度額
+    carry_over DECIMAL(13, 0) DEFAULT 0,    #繰り越し分
     PRIMARY KEY(id), 
     INDEX(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -41,15 +43,15 @@ CREATE TABLE m_employees (
     INDEX(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-#T与信テーブル
+/* #T与信テーブル
 CREATE TABLE t_credits (
     no INT NOT NULL AUTO_INCREMENT,  #No
     id INT(8) NOT NULL, #顧客ID -r顧客
-    credit_limit DECIMAL(13, 0) NOT NULL DEFAULT 20000000,  #与信限度額
+    
     PRIMARY KEY(no, id), 
     INDEX(no, id), 
     FOREIGN KEY (id) REFERENCES m_customers(id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8; */
 
 #T買注文テーブル（受注）
 CREATE TABLE t_orders (
@@ -65,6 +67,10 @@ CREATE TABLE t_orders (
     transmission INT DEFAULT 1, #変更機構の種別
     note TEXT,  #備考
     received_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,    #受注日
+    cleared BOOLEAN NOT NULL DEFAULT FALSE, #受注消込
+    contracted DECIMAL(13, 0) DEFAULT NULL, #落札価格
+    expenses DECIMAL(13, 0) DEFAULT NULL,   #オークション諸経費
+    commision DECIMAL(13, 0) DEFAULT NULL,  #買手数料
     PRIMARY KEY(no), 
     INDEX(no), 
     FOREIGN KEY (customer) REFERENCES m_customers(id),
@@ -90,6 +96,25 @@ CREATE TABLE t_ordered_delivered (
     INDEX(id),
     FOREIGN KEY (order_no) REFERENCES t_orders(no), 
     FOREIGN KEY (deliverable_no) REFERENCES t_deliverables(no)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/* #繰り越し分
+CREATE TABLE t_carry_over (
+    
+) ENGINE=InnoDB DEFAULT CHARSET=utf8; */
+
+#T請求テーブル
+CREATE TABLE t_bills (
+    no INT NOT NULL AUTO_INCREMENT, #no
+    printed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, #発行日
+    cleared BOOLEAN NOT NULL DEFAULT FALSE, #請求消込
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#T請求-納品テーブル
+CREATE TABLE t_billed_delivered (
+    no INT NOT NULL AUTO_INCREMENT, #no
+    bill_no INT NOT NULL,   #請求No
+    deliverable_no INT NOT NULL,    #納品No
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 #終了
