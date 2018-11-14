9ede6783caa081f5766e014766d4767cf980f3a8
sen <kawahara.hal@gmail.com>
2018-11-14 13:50:28 +0900

    自社情報インサート文追加

---
+++ Sql/Dummy.sql
@@ -0,0 +1,15 @@
+INSERT INTO m_our_company(
+    phonetic,
+    name,
+    zip_code,
+    address,
+    phone,
+    fax
+) VALUES(
+    "ｽｸﾞｸﾙｼｬﾊﾝｶﾌﾞｼｷｶﾞｲｼｬ",
+    "スグクル社販株式会社",
+    "530-0001",
+    "大阪府大阪市北区梅田３丁目３−１",
+    "06-6347-0001",
+    "06-6345-3456"
+);
