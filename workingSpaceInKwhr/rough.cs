34a6ee2180921f680588509879692acab7240de5
sen <kawahara.hal@gmail.com>
2018-11-19 00:21:52 +0900

    納品のエンティティ追加

--- workingSpaceInKwhr/rough.cs
+++ workingSpaceInKwhr/rough.cs
@@ -2,40 +2,16 @@
 /*-------------------------------------------------*/
 private int _no; //主キー
 private Customers _customer; //顧客ID
-private Employees _rep; //営業担当者(社員ID)
-private String _carName; //車両名
-private String _carYear; //年式
-private String _carColor; //色
-private double _carMileage; //走行距離
-private int _budget; //予算
-private Boolean _budgetCategory;　//予算の振れ幅（true:付近、false:以下)
-private int _transmission; //変更機構の種別
-private String _note; //備考
-private String _receivedAt; //受注日
-private Boolean _cleared; //受注消込
-private int _contracted; //落札価格
-private int _expenses; //オークション諸経費
-private int _commision; //買手数料
+private string _printedAt; //発行日
 /*-------------------------------------------------*/
 
 /*コンストラクタ*/
 /*-------------------------------------------------*/
-this._no = 0;
-this._customer = new Customers();
-this._rep = new Employees();
-this._carName = "";
-this._carYear = "";
-this._carColor = "";
-this._carMileage = 0;
-this._budget = 0;
-this._budgetCategory = false;
-this._transmission = 1;
-this._note = "";
-this._receivedAt = "";
-this._cleared = false;
-this._contracted = 0;
-this._expenses = 0;
-this._commision = 0;
+public Deliverables() {
+    this._no = 0;
+    this._customer = new Customers();
+    this._printedAt = "";
+}
 /*-------------------------------------------------*/
 
 /*セッター*/
@@ -44,71 +20,19 @@ this._commision = 0;
 public void setNo(int no) {
     this._no = no;
 }
-public void setNo(String no) {
+public void setNo(string no) {
     this._no = int.Parse(no);
 }
 //顧客ID
 public void setCustomer(int customer) {
     this._customer.setId(customer);
 }
-public void setCustomer(String customer) {
+public void setCustomer(string customer) {
     this._customer.setId(customer);
 }
-//営業担当者（社員ID）
-public void setRep(String rep) {
-    this._rep.setId(rep);
-}
-//車両名
-public void setCarName(String carName) {
-    this._carName = carName;
-}
-//年式
-public void setCarYear(String carYear) {
-    this._carYear = carYear;
-}
-//色
-public void setCarColor(String carColor) {
-    this._carColor = carColor;
-}
-//走行距離
-public void setCarMileage(double carMileage) {
-    this._carMileage = carMileage;
-}
-//予算
-public void setBudget(int budget) {
-    this._budget = budget;
-}
-//予算の振れ幅
-public void setBudgetCategory(Boolean budgetCategory) {
-    this._budgetCategory = budgetCategory;
-}
-//変更機構の種別
-public void setTransmission(int transmission) {
-    this._transmission = transmission;
-}
-//備考
-public void setNote(String note) {
-    this._note = note;
-}
-//受注日
-public void setReceivedAt(String receivedAt) {
-    this._receivedAt = receivedAt;
-}
-//受注消込
-public void setCleared(Boolean cleared) {
-    this._cleared = cleared;
-}
-//落札価格
-public void setContracted(int contracted) {
-    this._contracted = contracted;
-}
-//オークション諸経費
-public void setExpenses(int expenses) {
-    this._expenses = expenses;
-}
-//買手数料
-public void setCommision(int commision) {
-    this._commision = commision;
+//発行日
+public void setPrintedAt(string printedAt) {
+    this._printedAt = printedAt;
 }
 /*-------------------------------------------------*/
 
@@ -119,63 +43,11 @@ public int getNo() {
     return this._no;
 }
 //顧客ID
-public Customers getCustomer() {
+public int getCustomer() {
     return this._customer.getId();
 }
-//営業担当者（社員ID）
-public Employees getRep() {
-    return this._rep.getId();
-}
-//車両名
-public String getCarName() {
-    return this._carName;
-}
-//年式
-public String getCarYear() {
-    return this._carYear;
-}
-//色
-public String getCarColor() {
-    return this._carColor;
-}
-//走行距離
-public double getCarMileage() {
-    return this._carMileage;
-}
-//予算
-public int getBudget() {
-    return this._budget;
-}
-//予算の振れ幅
-public Boolean isAround() {
-    return this._budgetCategory;
-}
-//変更機構の種別
-public int getTransmission() {
-    return this._transmission;
-}
-//備考
-public String getNote() {
-    return this._note;
-}
-//受注日
-public String getReceivedAt() {
-    return this._receivedAt;
-}
-//受注消込
-public Boolean isCleared() {
-    return this._cleared;
-}
-//落札価格
-public int getContracted() {
-    return this._contracted;
-}
-//オークション諸経費
-public int getExpenses() {
-    return this._expenses;
-}
-//買手数料
-public int getCommision() {
-    return this._commision;
+//発行日
+public string getPrintedAt() {
+    return this._printedAt;
 }
 /*-------------------------------------------------*/
