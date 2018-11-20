818eacfafef0dc156f6b3e9e3907737f0183d20f
WIN10OHS60340\ohs60340 <aaa@bbb.com>
2018-11-20 15:25:47 +0900

    ログイン

--- Project/IHWork/IHWork/login.cs
+++ Project/IHWork/IHWork/login.cs
@@ -12,9 +12,14 @@ namespace IHWork
 {
     public partial class login : Form
     {
+        private String _dbCnctStr;   // DB接続文字列を格納するフィールド。
+        MySqlConnection _cnct;
+
         public login()
         {
             InitializeComponent();
         }
+
+        
     }
 }
