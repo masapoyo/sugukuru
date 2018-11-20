89cfe9697f931a5ae2cdf3113633a20ba6c4b75e
Yuki Yoshida <ohs60319@osaka.hal.ac.jp>
2018-11-20 15:10:23 +0900

    作業分コミット

--- Project/IHWork/IHWork/paymentReference.cs
+++ Project/IHWork/IHWork/paymentReference.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace IHWork
 {
     public partial class paymentReference : Form
     {
+        private String _fileName = "";  //読み込んだファイル名。
+        private List<String> _bankDataList;
+
         public paymentReference()
         {
             InitializeComponent();
@@ -19,7 +23,14 @@ namespace IHWork
 
         private void oFDReference_FileOk(object sender, CancelEventArgs e)
         {
-         
+            //ファイルをList<String>に変換
+            StreamReader reader = new StreamReader(this._fileName, Encoding.GetEncoding("utf-8"));
+            this._bankDataList.Add(reader.ReadLine());
+            while(!reader.EndOfStream)
+            {
+                this._bankDataList.Add(reader.ReadLine());
+            }
+            reader.Close();
         }
 
         private void btReference_Click(object sender, EventArgs e)
@@ -27,7 +38,8 @@ namespace IHWork
             DialogResult dr = oFDReference.ShowDialog();
             if (dr == System.Windows.Forms.DialogResult.OK)
             {
-                tbReference.Text = oFDReference.FileName;
+                this._fileName = oFDReference.FileName;
+                tbReference.Text = this._fileName;
             }
         }
     }
