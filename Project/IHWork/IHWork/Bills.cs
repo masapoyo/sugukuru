aeff662b9c7f2d68bacbda83b1383fb759f6a603
sen <kawahara.hal@gmail.com>
2018-11-20 15:16:29 +0900

    エンティティ呼び込み

---
+++ Project/IHWork/IHWork/Bills.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    class Bills
+    {
+        /*フィールド*/
+        /*-------------------------------------------------*/
+        private int _no; //主キー
+        private string _printedAt; //発行日
+        private Boolean _cleared; //請求消込
+        /*-------------------------------------------------*/
+
+        /*コンストラクタ*/
+        /*-------------------------------------------------*/
+        public Bills()
+        {
+            this._no = 0;
+            this._printedAt = "";
+            this._cleared = false;
+        }
+        /*-------------------------------------------------*/
+
+        /*セッター*/
+        /*-------------------------------------------------*/
+        //主キー
+        public void setNo(int no)
+        {
+            this._no = no;
+        }
+        public void setNo(string no)
+        {
+            this._no = int.Parse(no);
+        }
+        //発行日
+        public void setPrintedAt(string printedAt)
+        {
+            this._printedAt = printedAt;
+        }
+        //請求消込
+        public void setCleared(Boolean cleared)
+        {
+            this._cleared = cleared;
+        }
+        /*-------------------------------------------------*/
+
+        /*ゲッター*/
+        /*-------------------------------------------------*/
+        //主キー
+        public int getNo()
+        {
+            return this._no;
+        }
+        //受注No
+        public string getPrintedAt()
+        {
+            return this._printedAt;
+        }
+        //納品No
+        public Boolean isCleared()
+        {
+            return this._cleared;
+        }
+        /*-------------------------------------------------*/
+    }
+}
---
+++ Project/IHWork/IHWork/Deliverables.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    class Deliverables
+    {
+        /*フィールド*/
+        /*-------------------------------------------------*/
+        private int _no; //主キー
+        private Customers _customer; //顧客ID
+        private string _printedAt; //発行日
+                                   /*-------------------------------------------------*/
+
+        /*コンストラクタ*/
+        /*-------------------------------------------------*/
+        public Deliverables()
+        {
+            this._no = 0;
+            this._customer = new Customers();
+            this._printedAt = "";
+        }
+        /*-------------------------------------------------*/
+
+        /*セッター*/
+        /*-------------------------------------------------*/
+        //主キー
+        public void setNo(int no)
+        {
+            this._no = no;
+        }
+        public void setNo(string no)
+        {
+            this._no = int.Parse(no);
+        }
+        //顧客ID
+        public void setCustomer(int customer)
+        {
+            this._customer.setId(customer);
+        }
+        public void setCustomer(string customer)
+        {
+            this._customer.setId(customer);
+        }
+        //発行日
+        public void setPrintedAt(string printedAt)
+        {
+            this._printedAt = printedAt;
+        }
+        /*-------------------------------------------------*/
+
+        /*ゲッター*/
+        /*-------------------------------------------------*/
+        //主キー
+        public int getNo()
+        {
+            return this._no;
+        }
+        //顧客ID
+        public int getCustomer()
+        {
+            return this._customer.getId();
+        }
+        //発行日
+        public string getPrintedAt()
+        {
+            return this._printedAt;
+        }
+        /*-------------------------------------------------*/
+    }
+}
