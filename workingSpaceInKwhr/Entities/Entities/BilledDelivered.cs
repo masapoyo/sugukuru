586f49f0b1a23b60cc0fe31783b1d63fe0c286b8
sen <kawahara.hal@gmail.com>
2018-11-20 15:02:44 +0900

    エンティティクラス　完了

---
+++ workingSpaceInKwhr/Entities/Entities/BilledDelivered.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    class BilledDelivered
+    {
+        /*フィールド*/
+        /*-------------------------------------------------*/
+        private int _no; //主キー
+        private Deliverables _deliverableNo; //納品No
+        private Bills _billNo; //請求No
+        /*-------------------------------------------------*/
+
+        /*コンストラクタ*/
+        /*-------------------------------------------------*/
+        public BilledDelivered()
+        {
+            this._no = 0;
+            this._deliverableNo = new Deliverables();
+            this._billNo = new Bills();
+        }
+        /*-------------------------------------------------*/
+
+        /*セッター*/
+        /*-------------------------------------------------*/
+        //主キー
+        public void setNo(int no)
+        {
+            this._no = no;
+        }
+        public void setNo(string no)
+        {
+            this._no = int.Parse(no);
+        }
+        //納品No
+        public void setDeliveravleNo(int deliverableNo)
+        {
+            this._deliverableNo.setNo(deliverableNo);
+        }
+        public void setDeliveravleNo(string deliverableNo)
+        {
+            this._deliverableNo.setNo(deliverableNo);
+        }
+        //請求No
+        public void setBillNo(int billNo)
+        {
+            this._billNo.setNo(billNo);
+        }
+        public void setBillNo(string billNo)
+        {
+            this._billNo.setNo(billNo);
+        }
+        /*-------------------------------------------------*/
+
+        /*ゲッター*/
+        /*-------------------------------------------------*/
+        //主キー
+        public int getNo()
+        {
+            return this._no;
+        }
+        //納品No
+        public int getDeliverableNo()
+        {
+            return this._deliverableNo.getNo();
+        }
+        //請求No
+        public int getBillNo()
+        {
+            return this._billNo.getNo();
+        }
+        /*-------------------------------------------------*/
+    }
+}
--- workingSpaceInKwhr/rough.cs
+++ workingSpaceInKwhr/rough.cs
@@ -1,53 +1,25 @@
 /*フィールド*/
 /*-------------------------------------------------*/
 private int _no; //主キー
-private Customers _customer; //顧客ID
-private string _printedAt; //発行日
+//納品No
+//請求No
 /*-------------------------------------------------*/
 
 /*コンストラクタ*/
 /*-------------------------------------------------*/
-public Deliverables() {
-    this._no = 0;
-    this._customer = new Customers();
-    this._printedAt = "";
+public BilledDelivered() {
+
 }
 /*-------------------------------------------------*/
 
 /*セッター*/
 /*-------------------------------------------------*/
 //主キー
-public void setNo(int no) {
-    this._no = no;
-}
-public void setNo(string no) {
-    this._no = int.Parse(no);
-}
-//顧客ID
-public void setCustomer(int customer) {
-    this._customer.setId(customer);
-}
-public void setCustomer(string customer) {
-    this._customer.setId(customer);
-}
-//発行日
-public void setPrintedAt(string printedAt) {
-    this._printedAt = printedAt;
-}
+
 /*-------------------------------------------------*/
 
 /*ゲッター*/
 /*-------------------------------------------------*/
 //主キー
-public int getNo() {
-    return this._no;
-}
-//顧客ID
-public int getCustomer() {
-    return this._customer.getId();
-}
-//発行日
-public string getPrintedAt() {
-    return this._printedAt;
-}
+
 /*-------------------------------------------------*/
